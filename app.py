69e7c966cdf5518bf76f479ae0c92556c8b206b3
rishu <divyanshkumarworks@gmail.com>
2023-07-28 01:47:40 +0530

    search all users function created

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
+from bson.json_util import dumps
 
 app = Flask(__name__)
 
 client = MongoClient("mongodb://localhost:27017/")
 print(client)
 Users = client["Users"]
-Porfile = Users["profile"]
+Profile = Users["profile"]
 
 @app.route("/users", methods=["POST"])
 def create_new_user():
@@ -17,8 +18,13 @@ def create_new_user():
 	password = json["password"]
 	if request.method == "POST" and id and name and email and password:
 		post = {"_id": id, "name": name, "email": email, "password": password}
-		Porfile.insert_one(post)
+		Profile.insert_one(post)
 		return jsonify("user created sucessfully")
+
+@app.route("/users")
+def users():
+	users = Profile.find()
+	return dumps(users)
 
 
 if __name__ == "__main__":
