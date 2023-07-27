83083f54e58d6a38ea7c7e0793d7c0ad15a99cef
rishu <divyanshkumarworks@gmail.com>
2023-07-28 01:30:46 +0530

    create new user function added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -7,6 +7,19 @@ client = MongoClient("mongodb://localhost:27017/")
 print(client)
 Users = client["Users"]
 Porfile = Users["profile"]
+
+@app.route("/users", methods=["POST"])
+def create_new_user():
+	json = request.json
+	id = json["id"]
+	name = json["name"]
+	email = json["email"]
+	password = json["password"]
+	if request.method == "POST" and id and name and email and password:
+		post = {"_id": id, "name": name, "email": email, "password": password}
+		Porfile.insert_one(post)
+		return jsonify("user created sucessfully")
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
