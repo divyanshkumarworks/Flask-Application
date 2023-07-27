55e51f08b51fe0fff195e09d82e39e5cf1b127d6
rishu <divyanshkumarworks@gmail.com>
2023-07-28 01:16:05 +0530

    created mongodb connections

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+client = MongoClient("mongodb://localhost:27017/")
+print(client)
+Users = client["Users"]
+Porfile = Users["profile"]
 
 if __name__ == "__main__":
 	app.run(debug=True)
+	
+
